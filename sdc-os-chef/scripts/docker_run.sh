7c9c225c7675b042c52656478c8b9f3ae83c9014
Alexis de Talhouët <adetalhouet89@gmail.com>
2017-03-10 09:11:05 -0500

    Local successfull SDC run
    1. Compile the SDC project: mvn clean install
    2. In sdc-os-chef/scripts/docker_run.sh change the IP address to
    your host IP.
    3. AUTO.json contains hard-coded values for the containers that will
    be spawned, make sure those are accurate.
    4. Let your docker engine have at least 10 GO of RAM
    5. Build the images:
    	cd sdc-os-chef
    	mvn clean install -Pdocker
    6. Change MsgRtrApi.properties to reflect the IPs of ZK and Kafka
    7. Run ./sdc-os-chef/scripts/docker_run.sh
    
    Change-Id: I9f7e1086c5f7a79e8f404e73457528efbb64ee0f
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- sdc-os-chef/scripts/docker_run.sh
+++ sdc-os-chef/scripts/docker_run.sh
@@ -8,7 +8,7 @@ function usage {
 
 function cleanup {
 	echo "performing old dockers cleanup"
-	docker_ids=`docker ps -a | egrep "ecomp-nexus:${PORT}/sdc|sdc|Exit" | awk '{print $1}'`
+	docker_ids=`docker ps -q`
 	for X in ${docker_ids}
 	do
 	   docker rm -f ${X}
@@ -17,14 +17,14 @@ function cleanup {
 
 
 function dir_perms {
-	mkdir -p /data/logs/BE/ASDC/ASDC-BE
-	mkdir -p /data/logs/FE/ASDC/ASDC-FE
-	chmod -R 777 /data/logs
+	mkdir -p data-logs/BE/ASDC/ASDC-BE
+	mkdir -p data-logs/FE/ASDC/ASDC-FE
+	chmod -R 777 data-logs
 }
 
 
 RELEASE=latest
-[ -f /opt/config/env_name.txt ] && DEP_ENV=$(cat /opt/config/env_name.txt) || DEP_ENV=__ENV-NAME__
+[ -f /opt/config/env_name.txt ] && DEP_ENV=$(cat /opt/config/env_name.txt) || DEP_ENV=AUTO
 [ -f /opt/config/nexus_username.txt ] && NEXUS_USERNAME=$(cat /opt/config/nexus_username.txt)    || NEXUS_USERNAME=release
 [ -f /opt/config/nexus_password.txt ] && NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)      || NEXUS_PASSWD=sfWU3DFVdBr7GVxB85mTYgAW
 [ -f /opt/config/nexus_docker_repo.txt ] && NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt) || NEXUS_DOCKER_REPO=ecomp-nexus:${PORT}
@@ -60,20 +60,41 @@ done
 cleanup
 
 
-export IP=`ifconfig eth0 | awk -F: '/inet addr/ {gsub(/ .*/,"",$2); print $2}'`
+export IP=192.168.36.233
 
 echo ""
 
+# running zookeeper
+echo "docker run zookeeper..."
+docker run --detach --name zookeeper -log-driver=json-file --log-opt max-size=30m --log-opt max-file=5 --volume ~/temp/sdc/sdc-os-chef/scripts/docker_files/data-zookeeper:/opt/zookeeper-3.4.9/data --publish 2181:2181 wurstmeister/zookeeper
+
+# running kafka
+echo "docker run kafka..."
+docker run --detach --name kafka --env KAFKA_ADVERTISED_HOST_NAME=kafka --env KAFKA_ZOOKEEPER_CONNECT=172.17.0.2:2181 --env KAFKA_BROKER_ID=1 -log-driver=json-file --log-opt max-size=30m --log-opt max-file=5 --volume /var/run/docker.sock:/var/run/docker.sock --volume ~/temp/sdc/sdc-os-chef/scripts/data/logs/kafka:/kafka --volume ~/temp/sdc/scripts/sdc-os-chef/docker_files/start-kafka.sh:/start-kafka.sh --publish 9092:9092 dockerfiles_kafka 
+
+# running dmaap
+echo "docker run dmaap.."
+docker run --detach --name dmaap -log-driver=json-file --log-opt max-size=30m --log-opt max-file=5 --volume ~/temp/sdc/sdc-os-chef/scripts/docker_files/MsgRtrApi.properties:/appl/dmaapMR1/bundleconfig/etc/appprops/MsgRtrApi.properties --volume ~/temp/sdc/sdc-os-chef/scripts/docker_files/cadi.properties:/appl/dmaapMR1/etc/cadi.properties --volume ~/temp/sdc/sdc-os-chef/scripts/docker_files/mykey:/appl/dmaapMR1/etc/keyfile --publish 3904:3904 --publish 3905:3905 attos/dmaap
+
+echo "please wait while dmaap is starting..."
+echo ""
+c=30 # seconds to wait
+REWRITE="\e[25D\e[1A\e[K"
+while [ $c -gt 0 ]; do
+    c=$((c-1))
+    sleep 1
+    echo -e "${REWRITE}$c"
+done
+echo -e ""
+
 # Elastic-Search
 echo "docker run sdc-elasticsearch..."
-docker pull ${NEXUS_DOCKER_REPO}/openecomp/sdc-elasticsearch:${RELEASE}
-docker run --detach --name sdc-es --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --memory 1g --memory-swap=1g --ulimit memlock=-1:-1 --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro -e ES_HEAP_SIZE=1024M --volume /data/ES:/usr/share/elasticsearch/data --volume /data/environments:/root/chef-solo/environments --publish 9200:9200 --publish 9300:9300 ${NEXUS_DOCKER_REPO}/openecomp/sdc-elasticsearch:${RELEASE}
+docker run --detach --name sdc-es --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --memory 1g --memory-swap=1g --ulimit memlock=-1:-1 --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro -e ES_HEAP_SIZE=1024M --volume ~/temp/sdc/sdc-os-chef/data/logs/ES:/usr/share/elasticsearch/data --volume ~/temp/sdc/sdc-os-chef/environments:/root/chef-solo/environments --publish 9200:9200 --publish 9300:9300 openecomp/sdc-elasticsearch:latest
 
 
 # cassandra
 echo "docker run sdc-cassandra..."
-docker pull ${NEXUS_DOCKER_REPO}/openecomp/sdc-cassandra:${RELEASE}
-docker run --detach --name sdc-cs --env ENVNAME="${DEP_ENV}" --env HOST_IP=${IP} --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro --volume /data/CS:/var/lib/cassandra --volume /data/environments:/root/chef-solo/environments --publish 9042:9042 --publish 9160:9160 ${NEXUS_DOCKER_REPO}/openecomp/sdc-cassandra:${RELEASE}
+docker run --detach --name sdc-cs --env ENVNAME="${DEP_ENV}" --env HOST_IP=${IP} --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro --volume data-CS:/var/lib/cassandra --volume ~/temp/sdc/sdc-os-chef/environments:/root/chef-solo/environments --publish 9042:9042 --publish 9160:9160 openecomp/sdc-cassandra:latest
 
 
 echo "please wait while CS is starting..."
@@ -90,15 +111,13 @@ echo -e ""
 
 # kibana
 echo "docker run sdc-kibana..."
-docker pull ${NEXUS_DOCKER_REPO}/openecomp/sdc-kibana:${RELEASE}
-docker run --detach --name sdc-kbn --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --memory 2g --memory-swap=2g --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro --volume /data/environments:/root/chef-solo/environments --publish 5601:5601 ${NEXUS_DOCKER_REPO}/openecomp/sdc-kibana:${RELEASE}
+docker run --detach --name sdc-kbn --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --memory 2g --memory-swap=2g --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro --volume ~/temp/sdc/sdc-os-chef/environments:/root/chef-solo/environments --publish 5601:5601 openecomp/sdc-kibana:latest
 
 dir_perms
 
 # Back-End
 echo "docker run sdc-backend..."
-docker pull ${NEXUS_DOCKER_REPO}/openecomp/sdc-backend:${RELEASE}
-docker run --detach --name sdc-BE --env HOST_IP=${IP} --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --memory 4g --memory-swap=4g --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro --volume /data/logs/BE/:/var/lib/jetty/logs  --volume /data/environments:/root/chef-solo/environments --publish 8443:8443 --publish 8080:8080 ${NEXUS_DOCKER_REPO}/openecomp/sdc-backend:${RELEASE}
+docker run --detach --name sdc-BE --env HOST_IP=${IP} --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --memory 4g --memory-swap=4g --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro --volume ~/temp/sdc/sdc-os-chef/data/logs/BE:/var/lib/jetty/logs  --volume ~/temp/sdc/sdc-os-chef/environments:/root/chef-solo/environments --publish 8443:8443 --publish 8080:8080 openecomp/sdc-backend:latest
 
 echo "please wait while BE is starting..."
 echo ""
@@ -114,8 +133,7 @@ echo -e ""
 
 # Front-End
 echo "docker run sdc-frontend..."
-docker pull ${NEXUS_DOCKER_REPO}/openecomp/sdc-frontend:${RELEASE}
-docker run --detach --name sdc-FE --env HOST_IP=${IP} --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --memory 2g --memory-swap=2g --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro  --volume /data/logs/FE/:/var/lib/jetty/logs --volume /data/environments:/root/chef-solo/environments --publish 9443:9443 --publish 8181:8181 ${NEXUS_DOCKER_REPO}/openecomp/sdc-frontend:${RELEASE}
+docker run --detach --name sdc-FE --env HOST_IP=${IP} --env ENVNAME="${DEP_ENV}" --log-driver=json-file --log-opt max-size=100m --log-opt max-file=10 --ulimit memlock=-1:-1 --memory 2g --memory-swap=2g --ulimit nofile=4096:100000 --volume /etc/localtime:/etc/localtime:ro  --volume ~/temp/sdc/sdc-os-chef/data/logs/FE:/var/lib/jetty/logs --volume ~/temp/sdc/sdc-os-chef/environments:/root/chef-solo/environments --publish 9443:9443 --publish 8181:8181 openecomp/sdc-frontend:latest
 
 
 
@@ -131,9 +149,8 @@ while [ $c -gt 0 ]; do
 done
 echo -e ""
 
-/data/scripts/docker_health.sh
+scripts/docker_health.sh
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
-
